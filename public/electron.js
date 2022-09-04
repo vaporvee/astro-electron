da756cc8995ee908450e49918f0052f6ccd633a2
Yannik Ain <yannik@vaporvee.com>
2022-09-04 17:28:03 +0200

    added electron-builder

--- public/electron.js
+++ public/electron.js
@@ -13,12 +13,14 @@ function createWindow() {
     },
   });
 
+  win.removeMenu();
+
   // and load the index.html of the app.
   // win.loadFile("index.html");
   win.loadURL(
     isDev
       ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
+      : `file://${path.join(__dirname, '../dist/index.html')}`
   );
   // Open the DevTools.
   if (isDev) {
