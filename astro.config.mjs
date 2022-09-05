b62d451a9df581a5a2552883a18c307381947c3e
cmod31 <yannik@vaporvee.com>
2022-09-05 16:42:13 +0200

    Some CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import turbolinks from '@astrojs/turbolinks';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [turbolinks()],
+    vite: {
+      ssr: {
+        external: ["svgo"],
+      },
+    },
+  })
--- public/electron.js
+++ public/electron.js
@@ -6,11 +6,12 @@ const isDev = require('electron-is-dev');
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
+    width: 400,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
+    resizable: false
   });
 
   win.removeMenu();
